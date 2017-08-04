8d184d006913df20da1d92f88aca703da741c36b
Steardliy <rachff@gmail.com>
2017-08-05 00:53:45 +0900

    source

---
+++ [A17]-StockpileForDisaster/source/Building/RestrictedStorage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace StockpileForDisaster
+{
+    class RestrictedStorage : Building_Storage
+    {
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            foreach (var a in base.GetGizmos())
+            {
+                yield return a;
+            }
+            EntityListComp comp = this.GetComp<EntityListComp>();
+            if(comp == null)
+            {
+                yield break;
+            }
+            foreach (var a in CopyPasteRestrictionSetting.CopyPasteRestrictSettingsGizmo(comp.filter))
+            {
+                yield return a;
+            }
+        }
+    }
+}
